d571fc6a3ff4638faca57ee114dc03b5cb9a9cb8
overpowerhero <71799083+overpowerhero@users.noreply.github.com>
2021-01-09 19:16:03 +0800

    Initial commit

---
+++ Assets/prototype2-HYC ver/script/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// tset github
+// shiouuu test
+//HYC test 1/08 10pm
+
+public class Animal : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody enemyRb;//lok 0108
+    private GameObject player;//lok 0108
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lok 0108-HYC
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Human");
+    }
+     
+    // Update is called once per frame
+    void Update()
+    {
+        //lok 0108
+        Vector3 lookDirection = (player.transform.position- transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed );//問老師速度怎麼調
+    }
+}
---
+++ Assets/prototype2-HYC ver/script/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 30;
+    public float horizontalInput;
+    public float verticalInput;
+    private int xRange = 20;
+    public GameObject flyFood;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        //limit bound
+        if (transform.position.x < -xRange) // too left
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange) // too right
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        //需要再加後面?
+        // Generate fly food
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(flyFood, transform.position, flyFood.transform.rotation);
+        }
+    }
+}
