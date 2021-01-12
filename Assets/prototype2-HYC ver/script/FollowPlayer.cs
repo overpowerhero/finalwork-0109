87739ceb67816aea38954a92c1d78b75fdd9832f
overpowerhero <71799083+overpowerhero@users.noreply.github.com>
2021-01-13 00:15:38 +0800

    1/13brokentest-12.15

--- Assets/prototype2-HYC ver/script/FollowPlayer.cs
+++ Assets/prototype2-HYC ver/script/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject Human;
-    private Vector3 offset = new Vector3(0, 45, -25);
+    private Vector3 offset = new Vector3(0, 25, -20);
     // Start is called before the first frame update
     void Start()
     {
